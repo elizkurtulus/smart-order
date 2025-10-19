15bb62837089bdb7ce3b4ed3070bdfc1cdd51dd6
elizkurtulus <elizkurtulus17@gmail.com>
2025-10-19 16:07:01 +0300

    Edited docker-compose.yml

--- product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
+++ product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
@@ -1,12 +1,12 @@
-package main.java.com.turkcell.product_service.controller;
+package com.turkcell.product_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/products")
