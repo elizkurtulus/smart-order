ff5d5737be12843fbe36bb1a1a389140999c49e5
elizkurtulus <elizkurtulus17@gmail.com>
2025-10-19 12:49:34 +0300

    edited application

--- product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
+++ product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
@@ -1,34 +1,38 @@
 package main.java.com.turkcell.product_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductsController {
+    @GetMapping
+    public String getProducts() {
+        return "Products";
+    }
 
-  @GetMapping
-  public String getProducts() {
-    return "Products";
-  }
+    @GetMapping("{id}")
+    public String getProductById(@PathVariable String id) {
+        return "Product " + id;
+    }
 
-  @PostMapping
-  public String createProduct() {
-    return "Product created";
-  } 
+    @PostMapping
+    public String createProduct() {
+        return "Product created successfully";
+    }
 
-  @PutMapping()
-  public String updateProduct() {
-    return "Product updated";
-  }
+    @PutMapping
+    public String updateProduct() {
+        return "Product updated successfully";
+    }
 
-  @DeleteMapping()
-  public void deleteProduct() {
-    System.out.println("Product deleted");
-  }
-
+    @DeleteMapping
+    public String deleteProduct() {
+        return "Product deleted successfully";
+    }
 }
