d7b3fc987c889a044f46eb3e8a2be4d0bf6798bf
elizkurtulus <elizkurtulus17@gmail.com>
2025-11-04 17:29:16 +0300

    added config and controller for oauth2

--- bff-service/src/main/java/com/turkcell/bff_service/config/SecurityConfig.java
+++ bff-service/src/main/java/com/turkcell/bff_service/config/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.turkcell.bff_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFlux //arastirma odev: sprinq web ve spring reactive web farklari
+@EnableWebFluxSecurity
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .logout(l -> l.logoutUrl("/"))
+                .build();
+    }
 }
